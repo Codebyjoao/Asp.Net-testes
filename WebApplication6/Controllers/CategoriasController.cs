e0cee6770a73f24bf358bee112e34bf6bb0cb911
joao <joao.goncalves@cetemcba.com>
2023-03-01 18:08:14 -0400

    Cap 2

--- WebApplication6/Controllers/CategoriasController.cs
+++ WebApplication6/Controllers/CategoriasController.cs
@@ -26,17 +26,68 @@ namespace WebApplication6.Controllers
                 },
                 new Categoria() {
                 CategoriaId =   4,
+                Nome = "Mouses"
                 },
                 new Categoria() {
                 CategoriaId =   5,
-                 Nome    =   "Desktops"
+                Nome    =   "Desktops"
                 }
             };
 
         // GET: Categorias
         public ActionResult Index()
         {
-            return View(categorias);
+            return View(categorias.OrderBy(c => c.Nome));
+        }
+
+        // GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            categorias.Add(categoria);
+            categoria.CategoriaId = categorias.Select(m => m.CategoriaId).Max() + 1;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(long id)
+        {
+            return View(categorias.Where(
+                m => m.CategoriaId == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categoria categoria)
+        {
+            categorias.Remove(categorias.Where(
+                c => c.CategoriaId == categoria.CategoriaId).First());
+            categorias.Add(categoria);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(long id)
+        {
+            return View(categorias.Where(
+                m => m.CategoriaId == id).First());
+        }
+
+        public ActionResult Delete(long id)
+        {
+            return View(categorias.Where(m => m.CategoriaId == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete( Categoria categoria)
+        {
+            categorias.Remove(categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
+            return RedirectToAction("Index");
         }
     }
 }
