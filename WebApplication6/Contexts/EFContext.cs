55bc6a7e09a29b3afc7b0024330299d0d0ffae07
joao <joao.goncalves@cetemcba.com>
2023-03-02 18:01:53 -0400

    Cap 2.2

---
+++ WebApplication6/Contexts/EFContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication6.Models;
+
+namespace WebApplication6.Contexts
+{
+    public class EFContext : DbContext
+    {
+        public EFContext(): base("Asp_Net_MVS_CS") { }
+
+        public DbSet<Categoria>Categorias { get; set; }
+        public DbSet<Fabricante> Fabicates { get; set; }
+    }
+}
---
+++ WebApplication6/Controllers/FabricanteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication6.Contexts;
+
+namespace WebApplication6.Controllers
+{
+    public class FabricanteController : Controller
+    {
+        private EFContext context = new EFContext();
+
+        // GET: Fabricante
+        public ActionResult Index()
+        {
+            return View(context.Fabicates.OrderBy(c => c.Nome));
+        }
+    }
+}
